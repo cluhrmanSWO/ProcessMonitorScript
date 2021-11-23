f04140c3cbb4fad960b7ad00f9497d3b12324d3f
cluhrmanSWO <87212236+cluhrmanSWO@users.noreply.github.com>
2021-11-23 10:51:17 -0800

    Update procmon_script.ps1
    added .\ to procmon.exe lines
    moved $result variable

--- procmon_script.ps1
+++ procmon_script.ps1
@@ -2,29 +2,30 @@ $backingFile="LogFiles.PML"
 $pmcFile="ProcmonConfiguration.pmc"
 $csvTrace="LogFiles.csv"
 $csvOutput="allOutput.csv"
-$result = Get-Childitem -Path "..\" -Include $backingFile -Recurse -ErrorAction SilentlyContinue
 $pmlStatus = $true
 $procmon="Process Monitor"
 
 for ($i = 0; $i -lt 24; $i++){
 	Write-Host "Starting $procmon"
-	procmon.exe /Quiet /AcceptEula /BackingFile `"$backingFile`" /Minimized /LoadConfig `"$pmcFile`"
+	.\Procmon.exe /Quiet /AcceptEula /BackingFile `"$backingFile`" /Minimized /LoadConfig `"$pmcFile`"
 	Write-Host "$procmon is running"
 
 	Start-Sleep -s 30
 
 	Write-Host "Closing $procmon"
-	procmon.exe /Terminate
+	.\Procmon.exe /Terminate
 
 	while (Get-Process procmon -ErrorAction Ignore) {
 		Write-Host "Waiting on $procmon to exit..."
 		Start-Sleep -s 10
 	}
 
+
+	$result = Get-Childitem -Path "..\" -Include $backingFile -Recurse -ErrorAction SilentlyContinue
 	if ($result -eq $null){
 		Write-Host "You do not have PML file to export"
 	}else {
-		procmon.exe /SaveApplyFilter /OpenLog `"$backingFile`" /SaveAs `"$csvTrace`" 
+		.\Procmon.exe /SaveApplyFilter /OpenLog `"$backingFile`" /SaveAs `"$csvTrace`" 
 	}
 	
 	Write-Host "Waiting to write $csvTrace to $csvOutput"
